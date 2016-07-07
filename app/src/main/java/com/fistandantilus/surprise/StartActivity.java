91253723a14dfecf50a5b1005514cf076177f84b
Alexey Levadski <alevadski@gmail.com>
2016-07-07 19:48:24 +0300

    Initial commit

---
+++ app/src/main/java/com/fistandantilus/surprise/StartActivity.java
@@ -0,0 +1,23 @@
+package com.fistandantilus.surprise;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+    }
+
+    public void onLoginClick(View view) {
+
+    }
+
+    public void onSignUpClick(View view) {
+
+    }
+}
