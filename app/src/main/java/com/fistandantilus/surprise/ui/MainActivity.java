f620f1269385c8806382f57149613357ad9e9c4b
Alexey Levadski <alevadski@gmail.com>
2016-07-25 18:29:22 +0300

    created navigation drawer sign out now in drawer onBackPressed just closes app now

--- app/src/main/java/com/fistandantilus/surprise/ui/MainActivity.java
+++ app/src/main/java/com/fistandantilus/surprise/ui/MainActivity.java
@@ -4,13 +4,19 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.fistandantilus.surprise.R;
 import com.fistandantilus.surprise.dao.UserData;
@@ -24,7 +30,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener {
 
     public static final int READ_CONTACTS_PERMISSION_REQUEST_CODE = 111;
 
@@ -34,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     private String userUID;
     private UserData userData;
@@ -43,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle(null);
         initFirebase();
         initUI();
     }
@@ -87,29 +94,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 userData = dataSnapshot.getValue(UserData.class);
-
                 setTitle(userData.getEmail());
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
-
-
     }
 
     private void initUI() {
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        bindViews();
 
         setSupportActionBar(toolbar);
         setupViewPager();
         tabLayout.setupWithViewPager(viewPager);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        AppBarLayout.LayoutParams params = (AppBarLayout.LayoutParams) toolbar.getLayoutParams();
+        params.setScrollFlags(0);
+
+        viewPager.addOnPageChangeListener(this);
+    }
+
+    private void bindViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
     }
 
     private void setupViewPager() {
@@ -117,11 +137,6 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new FriendsFragment(), "FRIENDS");
         adapter.addFragment(new WallpapersFragment(), "WALLPAPERS");
         viewPager.setAdapter(adapter);
-    }
-
-    @Override
-    public void onBackPressed() {
-        signOut();
     }
 
     private void signOut() {
@@ -148,8 +163,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case READ_CONTACTS_PERMISSION_REQUEST_CODE: {
                 // If request is cancelled, the result arrays are empty.
@@ -169,5 +183,68 @@ public class MainActivity extends AppCompatActivity {
             // other 'case' lines to check for other
             // permissions this app might request
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        item.setChecked(true);
+
+        switch (item.getItemId()) {
+            case R.id.menu_item_people:
+                viewPager.setCurrentItem(0, true);
+                break;
+            case R.id.menu_item_wallpaper:
+                viewPager.setCurrentItem(1, true);
+                break;
+            case R.id.menu_item_log_out:
+                signOut();
+                break;
+        }
+
+        drawerLayout.closeDrawers();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+            return;
+        } else finishAffinity();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+        switch (position) {
+            case 0:
+                navigationView.setCheckedItem(R.id.menu_item_people);
+                break;
+            case 1:
+                navigationView.setCheckedItem(R.id.menu_item_wallpaper);
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+
     }
 }
